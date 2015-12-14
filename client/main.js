6de39c397c55eac1198c3b38bb0c69c87fe6ebe2
yswai <yswai1986@gmail.com>
2015-12-14 14:48:40 +0800

    bower

--- client/main.js
+++ client/main.js
@@ -1,7 +1,6 @@
 import angular from 'github:angular/bower-angular@1.4.8'
-import ngMaterial from 'angular-material'
 
-let app = angular.module('app', [ngMaterial]);
+let app = angular.module('app', []);
 app.controller('AppCtrl', function($scope) {
     let testVar = 'testVar';
     $scope.appState = {};
