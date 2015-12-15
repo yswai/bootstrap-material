5b2fcfddc288925a907cfbe985c30feaf18e5dac
yswai <yswai1986@gmail.com>
2015-12-15 14:13:13 +0800

    bootstrap load sequence

--- client/boot.js
+++ client/boot.js
@@ -3,10 +3,9 @@ import angular from 'github:angular/bower-angular@1.4.8'
 import ngUIRouter from 'angular-ui-router'
 import main from './main'
 
-import 'bootstrap/css/bootstrap.min.css!'
 import 'bootstrap-material/dist/css/bootstrap-material-design.min.css!'
 import 'bootstrap-material/dist/css/ripples.min.css!'
-import 'bootstrap';
+import 'bootstrap'
 import 'bootstrap-material'
 
 angular.element(document).ready(function() {
