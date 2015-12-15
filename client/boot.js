e208d1b7e2cea29bc65144202e3c26c581958113
yswai <yswai1986@gmail.com>
2015-12-15 12:02:54 +0800

    fixed ripple

--- client/boot.js
+++ client/boot.js
@@ -5,10 +5,9 @@ import main from './main'
 
 import 'bootstrap/css/bootstrap.min.css!'
 import 'bootstrap-material/dist/css/bootstrap-material-design.min.css!'
-import 'bootstrap';
 import 'bootstrap-material/dist/css/ripples.min.css!'
-import 'bootstrap-material/dist/js/material.min.js'
-import 'bootstrap-material/dist/js/ripples.min.js'
+import 'bootstrap';
+import 'bootstrap-material'
 
 angular.element(document).ready(function() {
     angular.bootstrap(document, [main.name]);
--- client/main.js
+++ client/main.js
@@ -1,10 +1,15 @@
 import angular from 'github:angular/bower-angular@1.4.8'
+import $ from 'jquery'
 import 'angular-ui-router'
 
 const main = angular.module('main', ['ui.router']);
 
 main.config(function($stateProvider) {
     console.log($stateProvider);
+});
+
+main.run(function() {
+    $.material.init();
 });
 
 main.controller('AppCtrl', function($scope) {
